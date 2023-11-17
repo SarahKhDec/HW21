4b15a8d8f920a27614f3d294c9d504f1e87905a5
Sarai <sarahkhanjari@gmail.com>
2023-11-17 06:41:17 +0330

    entity enums

---
+++ src/main/java/org/example/HW21/entity/enumeration/ExpertStatus.java
@@ -0,0 +1,8 @@
+package org.example.HW21.entity.enumeration;
+
+public enum ExpertStatus {
+    NEW,
+    AWAITING_CONFIRMATION,
+    ACCEPTED,
+    INACTIVE
+}
---
+++ src/main/java/org/example/HW21/entity/enumeration/OrderStatus.java
@@ -0,0 +1,10 @@
+package org.example.HW21.entity.enumeration;
+
+public enum OrderStatus {
+    WAITING_FOR_THE_SUGGESTION_OF_EXPERTS,
+    WAITING_FOR_SPECIALIST_SELECTION,
+    WAITING_FOR_THE_SPECIALIST_TO_COME_TO_YOUR_PLACE,
+    STARTED,
+    DONE,
+    PAID
+}
