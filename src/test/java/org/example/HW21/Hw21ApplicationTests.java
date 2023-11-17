811346f159dd05a6c8d20e7162448f5ab56d8903
Sarai <sarahkhanjari@gmail.com>
2023-11-17 06:43:59 +0330

    final version

---
+++ src/test/java/org/example/HW21/Hw21ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.HW21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
